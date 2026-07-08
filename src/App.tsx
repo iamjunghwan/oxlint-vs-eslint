6900857a84deea6b4d41eede6019c0875a5e94f5
iamjunghwan <dks3344@naver.com>
2026-07-08 12:06:19 +0900

    add oxlint-react-compiler-rules 🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ value }: { value: number }) {
+  const [count, setCount] = useState(0);
+  setCount(value);
 
   return (
     <>
@@ -116,7 +117,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
